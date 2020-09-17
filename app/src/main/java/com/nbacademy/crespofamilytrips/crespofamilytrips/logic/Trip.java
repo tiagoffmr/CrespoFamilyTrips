f9a92c609330edb79fc9f629a0d32e29742ced0d
Ricardo Faria <ricardoafonfaria@gmail.com>
2020-09-17 16:09:47 +0100

    First

---
+++ app/src/main/java/com/nbacademy/crespofamilytrips/crespofamilytrips/logic/Trip.java
@@ -0,0 +1,16 @@
+package com.nbacademy.crespofamilytrips.crespofamilytrips.logic;
+
+public class Trip {
+
+    private String name;
+    private String startDate;
+    private String endDate;
+    private int nPassengers;
+
+    public Trip() {
+
+    }
+
+
+
+}
