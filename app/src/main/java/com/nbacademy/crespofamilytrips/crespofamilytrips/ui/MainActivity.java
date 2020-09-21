db8d4e9c9416b591c8f2cb0d3186c109be9fac4f
Ricardo Faria <ricardoafonfaria@gmail.com>
2020-09-21 19:01:26 +0100

    Data models

--- app/src/main/java/com/nbacademy/crespofamilytrips/crespofamilytrips/ui/MainActivity.java
+++ app/src/main/java/com/nbacademy/crespofamilytrips/crespofamilytrips/ui/MainActivity.java
@@ -1,15 +1,21 @@
 package com.nbacademy.crespofamilytrips.crespofamilytrips.ui;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nbacademy.crespofamilytrips.crespofamilytrips.R;
+import com.nbacademy.crespofamilytrips.crespofamilytrips.logic.DBUtils;
 
 public class MainActivity extends AppCompatActivity {
+    DBUtils dbutils;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dbutils.onCreate(db);
         setContentView(R.layout.activity_main);
+
     }
 }
